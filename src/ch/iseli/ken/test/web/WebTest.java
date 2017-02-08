24a30aacb90ab5a1314144855bda7d72c166684b
Ken Iseli <ken.iseli@zuehlke.com>
2017-02-08 21:17:37 +0100

    Added a fully working WebTest for wikipedia using chrome browser (for simplicity with eclipse ide)

---
+++ src/ch/iseli/ken/test/web/WebTest.java
@@ -0,0 +1,79 @@
+package ch.iseli.ken.test.web;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class WebTest {
+	private static final String WIKIPEDIA_USERNAME = "kennethIzeli";
+	private static final String WIKIPEDIA_PASSWORD = "kennethIseli";
+
+	private WebDriver driver;
+
+	@Before
+	public void setUp() throws URISyntaxException {
+		ChromeOptions options = new ChromeOptions();
+		URL resource = getClass().getClassLoader().getResource("ch/iseli/ken/test/web/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", Paths.get(resource.toURI()).toString());
+		options.addArguments("start-maximized");
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability(ChromeOptions.CAPABILITY, options);
+		driver = new ChromeDriver(caps);
+	}
+
+	@Test
+	public void test() {
+		// Launch the Online Store Website
+		driver.get("http://www.wikipedia.org");
+
+		By wikipediaEnglishId = By.id("js-link-box-en");
+		WebElement englishWikipediaLink = driver.findElement(wikipediaEnglishId);
+		Assert.assertTrue(englishWikipediaLink.isDisplayed());
+		Assert.assertTrue(englishWikipediaLink.isEnabled());
+
+		// Let's go with english
+		englishWikipediaLink.click();
+
+		// Login
+		By loginLinkText = By.linkText("Log in");
+		WebElement loginLink = driver.findElement(loginLinkText);
+		Assert.assertTrue(loginLink.isDisplayed());
+		loginLink.click();
+
+		By usernameInputId = By.id("wpName1");
+		WebElement usernameInputField = driver.findElement(usernameInputId);
+		usernameInputField.sendKeys(WIKIPEDIA_USERNAME);
+
+		By passwordInputId = By.id("wpPassword1");
+		WebElement passwordInputField = driver.findElement(passwordInputId);
+		passwordInputField.sendKeys(WIKIPEDIA_PASSWORD);
+
+		passwordInputField.submit();
+
+		// Exactly two elements with class "new" in the header are to be present
+		By classNameNew = By.className("new");
+		List<WebElement> headerWithClassNewElements = driver.findElements(classNameNew);
+		Assert.assertEquals(headerWithClassNewElements.size(), 2);
+		for (WebElement headerWithClassNewElement : headerWithClassNewElements) {
+			Assert.assertTrue(headerWithClassNewElement.isDisplayed());
+			Assert.assertTrue(headerWithClassNewElement.isEnabled());
+		}
+
+		// Close the driver
+		driver.quit();
+
+	}
+
+}
