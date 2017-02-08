31bd74510aef20398dfec013a03c5d59bbfc19a6
Ken Iseli <ken.iseli@zuehlke.com>
2017-02-08 21:30:49 +0100

    Added multi platform capability. Also removed target, bin

--- src/ch/iseli/ken/test/web/WebTest.java
+++ src/ch/iseli/ken/test/web/WebTest.java
@@ -3,7 +3,6 @@ package ch.iseli.ken.test.web;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
-import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Assert;
@@ -17,6 +16,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class WebTest {
+	// TODO: Adjust this value according to your OS. See package ch.iseli.ken.test.web
+	private static final String CHROME_DRIVER_RESOURCE_PATH = "ch/iseli/ken/test/web/chromedriver_win32.exe";
+
 	private static final String WIKIPEDIA_USERNAME = "kennethIzeli";
 	private static final String WIKIPEDIA_PASSWORD = "kennethIseli";
 
@@ -25,7 +27,7 @@ public class WebTest {
 	@Before
 	public void setUp() throws URISyntaxException {
 		ChromeOptions options = new ChromeOptions();
-		URL resource = getClass().getClassLoader().getResource("ch/iseli/ken/test/web/chromedriver.exe");
+		URL resource = getClass().getClassLoader().getResource(CHROME_DRIVER_RESOURCE_PATH);
 		System.setProperty("webdriver.chrome.driver", Paths.get(resource.toURI()).toString());
 		options.addArguments("start-maximized");
 		DesiredCapabilities caps = new DesiredCapabilities();
@@ -34,7 +36,7 @@ public class WebTest {
 	}
 
 	@Test
-	public void test() {
+	public void testLoginToEnglishWikipedia() {
 		// Launch the Online Store Website
 		driver.get("http://www.wikipedia.org");
 
@@ -45,6 +47,7 @@ public class WebTest {
 
 		// Let's go with english
 		englishWikipediaLink.click();
+		Assert.assertTrue(driver.getCurrentUrl().contains("en.wikipedia.org"));
 
 		// Login
 		By loginLinkText = By.linkText("Log in");
@@ -62,10 +65,10 @@ public class WebTest {
 
 		passwordInputField.submit();
 
-		// Exactly two elements with class "new" in the header are to be present
+		// Exactly three elements with class "new" in the header are to be present
 		By classNameNew = By.className("new");
 		List<WebElement> headerWithClassNewElements = driver.findElements(classNameNew);
-		Assert.assertEquals(headerWithClassNewElements.size(), 2);
+		Assert.assertEquals(3, headerWithClassNewElements.size());
 		for (WebElement headerWithClassNewElement : headerWithClassNewElements) {
 			Assert.assertTrue(headerWithClassNewElement.isDisplayed());
 			Assert.assertTrue(headerWithClassNewElement.isEnabled());
